ece72a8da11e35dee46c2b3c46446f65865dd705
Łukasz Modzelewski <lukasz.modzelewski.pl@gmail.com>
2025-04-26 19:18:20 +0200

    added some greetings and preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dynamic-panda-c3083b.netlify.app/"
+  site: "https://dynamic-panda-c3083b.netlify.app/",
+  integrations: [preact()]
 });
